57832b2feb27dc4911eb9b1df2eaee7830174e8d
joshua <iuttn123@gmail.com>
2025-12-17 14:43:37 +0900

    ..

--- src/common/interceptor/error-format.interceptor.ts
+++ src/common/interceptor/error-format.interceptor.ts
@@ -10,6 +10,16 @@ import { catchError, Observable, throwError } from 'rxjs';
 @Injectable()
 export class ErrorFormatInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe();
+    return next.handle().pipe(
+      catchError((error) => {
+        return throwError(() => {
+          return {
+            statusCode: error.getStatus(),
+            success: false,
+            message: error.getResponse().message,
+          };
+        });
+      }),
+    );
   }
 }
